e7e96b04fb33684291664a47d4351794a930af00
beliefs22 <beliefs22@gmail.com>
2016-12-13 13:54:10 -0500

    I'm bad at git

--- runip.py
+++ runip.py
@@ -1,11 +1,12 @@
-import pulltimelineip
+import pulltimeline_working
 
 
 def main():
-    directories = pulltimelineip.
-    # pulltimelineip.getipdata.getLabs()
-    pulltimelineip.getipdata.getMeds()
-    # pulltimelineip.getipdata.getVitals()
+    directories = pulltimeline_working.getipdata.process.getDirectories()
+    ip_subject_files_dir = directories['IP Subject Dir']
+    pulltimeline_working.getipdata.getLabs(ip_subject_files_dir)
+    pulltimeline_working.getipdata.getMeds(ip_subject_files_dir)
+    pulltimeline_working.getipdata.getVitals(ip_subject_files_dir)
 
 if __name__ == "__main__":
     main()
