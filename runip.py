9887c4170652e3d053382818af5c03e7a1bcdec0
beliefs22 <beliefs22@gmail.com>
2016-12-21 18:02:00 -0500

    updated so it just runs the program instead of needing any information

--- runip.py
+++ runip.py
@@ -1,12 +1,7 @@
 import pulltimeline_working
 
-
 def main():
-    directories = pulltimeline_working.getipdata.process.getDirectories()
-    ip_subject_files_dir = directories['IP Subject Dir']
-    pulltimeline_working.getipdata.getLabs(ip_subject_files_dir)
-    pulltimeline_working.getipdata.getMeds(ip_subject_files_dir)
-    pulltimeline_working.getipdata.getVitals(ip_subject_files_dir)
+    pulltimeline_working.getipdata.getData()
 
 if __name__ == "__main__":
     main()
